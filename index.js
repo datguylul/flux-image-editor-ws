43551fab4351e66202b30fb3447ae6bfab493185
HOAG\Hoag <nghg.1223@gmail.com>
2024-11-03 22:17:32 +0700

    update

--- index.js
+++ index.js
@@ -3,67 +3,118 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
-const fluxWs = new WebSocket(
-  "wss://flux.longerthanthelongest.com/API/GenerateText2ImageWS",
-  {
-    headers: {
-      Authorization: "Basic Z3BwbToxMTE=",
-    },
-  }
-);
-
 const app = express();
-const port = 3000;
+const port = 8000;
 app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on("connection", (socket) => {
+const fluxGenerateRequest = {
+  prompt: "",
+  negativeprompt: "",
+  controlnetpreviewonly: false,
+  debugregionalprompting: false,
+  model: "flux1-dev-fp8",
+  images: "1",
+  seed: "-1",
+  steps: "20",
+  cfgscale: "7",
+  aspectratio: "1:1",
+  width: "1024",
+  height: "1024",
+  zeronegative: false,
+  seamlesstileable: "false",
+  initimagecreativity: "0.6",
+  initimageresettonorm: "0",
+  maskblur: "4",
+  maskgrow: "0",
+  initimagerecompositemask: true,
+  useinpaintingencode: false,
+  batchsize: "1",
+  saveintermediateimages: false,
+  donotsave: false,
+  nopreviews: false,
+  webhooks: "Normal",
+  internalbackendtype: "Any",
+  noseedincrement: false,
+  personalnote: "",
+  colordepth: "8bit",
+  modelspecificenhancements: true,
+  regionalobjectcleanupfactor: "0",
+  maskcompositeunthresholded: false,
+  savesegmentmask: false,
+  gligenmodel: "None",
+  cascadelatentcompression: "32",
+  removebackground: false,
+  shiftedlatentaverageinit: false,
+  automaticvae: true,
+  presets: [],
+  session_id: "6A6BCE6CDFAFA3B879BCA5185073F67ADF57C3E6",
+};
+
+io.sockets.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  socket.on("flux", (msg) => {
-    io.emit("flux", msg);
+  let fluxWs = null;
+
+  socket.on("flux-msg", (msg) => {
+    io.to(socket.id).emit("flux-msg", msg);
+  });
+
+  socket.on("flux-connect", () => {
+    try {
+      fluxWs = new WebSocket(
+        "wss://flux.longerthanthelongest.com/API/GenerateText2ImageWS",
+        {
+          headers: {
+            Authorization: "Basic Z3BwbToxMTE=",
+          },
+        }
+      );
+
+      fluxWs.on("open", function open() {
+        console.log("Connected to the Flux WebSocket server");
+        io.to(socket.id).emit("flux-connect", true);
+      });
+
+      fluxWs.on("message", function incoming(data) {
+        const message = data.toString();
+        try {
+          const parsedData = JSON.parse(message);
+          io.to(socket.id).emit("flux-msg", parsedData);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      });
+
+      fluxWs.on("close", function close(code, reason) {
+        console.log("fluxWs is closed:", code, reason);
+      });
+
+      io.to(socket.id).emit("flux-connect", true);
+    } catch (error) {
+      io.to(socket.id).emit("flux-connect", false);
+    }
+  });
+
+  socket.on("flux-generate", () => {
+    try {
+      const testMessage = JSON.stringify(fluxGenerateRequest);
+      fluxWs.send(testMessage, (err) => {
+        console.log("err", err);
+      });
+      io.to(socket.id).emit("flux-generate", true);
+    } catch (error) {
+      io.to(socket.id).emit("flux-generate", false);
+    }
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
+    fluxWs = null;
   });
-});
-
-fluxWs.on("open", function open() {
-  console.log("Connected to the Flux WebSocket server");
-});
-
-fluxWs.on("message", function incoming(data) {
-  const message = data.toString();
-  try {
-    const parsedData = JSON.parse(message);
-    io.emit("flux", parsedData);
-  } catch (error) {
-    console.error("Error parsing JSON:", error);
-  }
-});
-
-fluxWs.on("close", function close(code, reason) {
-  console.log("fluxWs is closed with code: " + code + " reason: " + reason);
-});
-
-fluxWs.on("error", function error(err) {
-  console.error("WebSocket error:", err);
 });
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-
-  app.post("/api/generate", (req, res) => {
-    const testMessage = JSON.stringify(req.body);
-    fluxWs.send(testMessage, (error) => {
-      if (error) {
-        console.error("Error sending message:", error);
-      } else {
-        console.log("Test message sent:", testMessage);
-      }
-    });
-    res.send("done");
-  });
 });
